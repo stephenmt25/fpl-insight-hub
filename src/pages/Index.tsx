428e3aa73685929066b3d0314b03add58c62596e
stephenmt <stephenmthomas95@gmail.com>
2025-01-15 15:04:17 +0530

    removing annoying pie chart

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -279,12 +279,13 @@ const Index = () => {
         </div>
         <div className="max-w-[94%] lg:max-w-full">
           <h3 className="text-lg font-medium mb-4">Data Visualization</h3>
-          <div className="grid gap-y-2 lg:grid-cols-2">
+          <div className="grid gap-2 lg:grid-cols-2">
             <div className="col-span-2 lg:col-span-1">
               <p className="text-gray-600">Overall Captaincy</p>
-              <ResponsiveContainer width="100%" height="100%">
+              {/* <ResponsiveContainer width="100%" height="100%">
                 <OverallCaptains />
-              </ResponsiveContainer>
+              </ResponsiveContainer> */}
+              <TempRadarChart />
             </div>
             <div className="col-span-2 lg:col-span-1">
               <p className="text-gray-600">Distribution</p>
